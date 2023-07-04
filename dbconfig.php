724e2af4d2ddc77a40c04e30247b2a25b1a329f1
102296 <33954729+102296@users.noreply.github.com>
2023-07-04 10:22:29 +0300

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+$dbservername = "localhost";
+$dbusername = "student";
+$dbpassword = "5trathm0re";
+$dbname = "bi1";
+
+/*
+Execute the following to create the bi1 database:
+bi1.sql
+*/
+?>
